f4e7cb153ad6ce275ba710369c888d1eca79bb22
NavalSharma7 <A00241484@myCambrian.ca>
2021-11-29 17:25:21 -0500

    first model added
    first model added

---
+++ ExpenseTrackerUnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExpenseTrackerUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
