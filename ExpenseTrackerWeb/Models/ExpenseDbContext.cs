e219ecf5bf1c87dd7db6e4f458e107941697014a
Naval Sharma <A00241484@mycambrian.ca>
2021-11-29 20:09:51 -0500

    db context added

---
+++ ExpenseTrackerWeb/Models/ExpenseDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseTrackerWeb.Models
+{
+    public class ExpenseDbContext : DbContext
+    {
+        public virtual DbSet<ExpenseReport> ExpenseReport { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=(localdb)\\projectsv13;Database=SecretDb;Trusted_Connection=True;MultipleActiveResultSets=true; Integrated Security = true;");
+            }
+        }
